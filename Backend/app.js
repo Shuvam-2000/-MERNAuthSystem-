51b31a8655c8e91f11fd17d21c384dd88a9211a6
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-12-26 16:38:30 +0530

    add: added routes for authentication and product display, added middlewares

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
-const express = require('express');
 require('./models/db');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./routes/AuthRouter')
+const ProductRouter = require('./routes/ProductRouter')
 
 // intialize express
 const app = express();
@@ -8,9 +12,17 @@ const app = express();
 // initalize the port
 const PORT = process.env.PORT || 8080;
 
-// route
+// middleware
+app.use(bodyParser.json());
+app.use(cors());  // Enable CORS for all routes
+
+// all routes here
 app.get('/', (req,res) => {
     res.send('Hello');
 })
+
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
 
 app.listen(PORT, () => console.log(`Server Started At PORT: ${PORT}`));
