6971daf57a663bfcd78be730d614e2b9bb800d97
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-12-28 16:35:17 +0530

    add: signup component client side validations and api fucntioanlities added

---
+++ Frontend/auth-mern-app/src/pages/SignUp.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from 'react-toastify';
+
+const SignUp = () => {
+
+    const navigate = useNavigate()
+    const [signUp, setsignUp] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleChange = (e) => {
+        setsignUp({...signUp, [e.target.name]: e.target.value});
+    }
+
+    const submitForm = async (e) => {
+        e.preventDefault()
+        if(!signUp.name){
+            toast.error('Please Enter Your Name');
+            return;
+        } else if (signUp.name.length <= 2 || signUp.name.length > 20) {
+            toast.error("Name Should Of 10 Letters At least");
+            return;
+        }
+        if (!signUp.email) {
+            toast.error("Please Enter Your Email");
+            return;
+        } else if (!/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/.test(signUp.email)) {
+            toast.error("Invalid Email Address");
+            return;
+        }
+        if (!signUp.password) {
+            toast.error("Please Enter Password");
+            return;
+        } else if (signUp.password.length >= 15 || signUp.password.length <= 5) {
+            toast.error("Password must be between 6 & 14 characters");
+            return;
+        } else if (!/[!@#$%^&*(),.?":{}|<>]/.test(signUp.password)) {
+            toast.error("Password must contain at least one special character");
+            return;
+        }else {
+            setsignUp({
+                name: "",
+                email: "",
+                password: "",        
+            })
+        }
+        try {
+            const url = 'http://localhost:8080/auth/signup';
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(signUp)
+            });
+            const result = await response.json();
+            // console.log(result); // debugging
+            const {success, message } = result;
+            if(success){
+                toast.success('Sign Up SuccesFull', message);
+                setTimeout(() => {
+                    navigate('/login')
+                },2000)
+            }
+        } catch (error) {
+            toast.error('SignUp Failed', error)
+        }
+        
+    }  
+
+  return (
+    <div className="container">
+      <h1>SignUp</h1>
+        <form onSubmit={submitForm}>
+            <div>
+                <label htmlFor="name">Name</label>
+                <input 
+                    onChange={handleChange}
+                    type="text"
+                    name="name" 
+                    value={signUp.name}
+                    placeholder="Enter Your Name"
+                    autoFocus
+                />
+            </div>
+            <div>
+                <label htmlFor="email">Email</label>
+                <input 
+                    onChange={handleChange}
+                    type="email"
+                    name="email" 
+                    value={signUp.email}
+                    placeholder="Enter Your Email"
+                />
+            </div>
+            <div>
+                <label htmlFor="password">Password</label>
+                <input 
+                    onChange={handleChange}
+                    type="password"
+                    name="password"
+                    value={signUp.password} 
+                    placeholder="Enter Your Password"
+                />
+            </div>
+            <button type="submit">Submit</button>
+            <span>Already Have an Account?
+                <Link to='/login'>Login</Link>
+            </span>
+        </form>
+        <ToastContainer />
+    </div>
+  )
+}
+
+export default SignUp;
