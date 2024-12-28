9b34f9ac94e7df6a1367de5a39530b591ef5a3de
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-12-28 16:32:46 +0530

    update: signup,login, home component and dynamic routing added

--- Frontend/auth-mern-app/src/App.jsx
+++ Frontend/auth-mern-app/src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
 import './App.css'
 
 function App() {
 
   return (
     <>
-     <h1>Auth App</h1> 
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navigate to='/login' />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </BrowserRouter> 
     </>
   )
 }
