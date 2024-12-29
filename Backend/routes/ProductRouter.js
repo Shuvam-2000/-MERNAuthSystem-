458979f5d5ef0652240949dd09874df3e8ac051b
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-12-29 16:41:30 +0530

    fixed: bugs fixed in product route

--- Backend/routes/ProductRouter.js
+++ Backend/routes/ProductRouter.js
@@ -6,10 +6,12 @@ router.get('/', ensureAuthenticated, (req,res) => {
     console.log("logged in user", req.user) // debugging
     res.status(200).json([
         {
+            id: 1,
             name: "mobile",
             price: 10000
         },
         {
+            id: 2,
             name: "tv",
             price: 50000
         }
