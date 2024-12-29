a31d39e1467ab75c26752814c99d14eec30c5ea2
Shuvam-2000 <shuvamsaha2000@gmail.com>
2024-12-29 16:42:04 +0530

    update: private routing to be added

--- Frontend/auth-mern-app/src/App.jsx
+++ Frontend/auth-mern-app/src/App.jsx
@@ -2,15 +2,22 @@ import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
+// import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  // const [ isAuthenticated, setisAunthenticated ] = useState(false)
+  // const PrivateRoute = ((e) => {
+  //   return isAuthenticated ? e : <Navigate to='/login' />
+  // })
 
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Navigate to='/login' />} />
+          {/* <Route path='/home' element={<PrivateRoute element={<Home />} />} /> */}
           <Route path='/home' element={<Home />} />
           <Route path='/signup' element={<SignUp />} />
           <Route path='/login' element={<Login />} />
